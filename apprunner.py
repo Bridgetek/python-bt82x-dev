4bbac6353169a9db6690721814168251e63b8108
Gordon McNab <gordon.mcnab@brtchip.com>
2025-06-30 09:30:27 +0100

    Recalculate Swapchain 0 based on bottom of reserved area and screen size.

--- apprunner.py
+++ apprunner.py
@@ -71,8 +71,8 @@ class run:
         if not minimal:
             scrsize = (surface.w * surface.h * 3)
             eve.CMD_REGWRITE(eve.REG_SC0_SIZE, 2)
-            eve.CMD_REGWRITE(eve.REG_SC0_PTR0, 0x7a00000)
-            eve.CMD_REGWRITE(eve.REG_SC0_PTR1, 0x7a00000 + scrsize)
+            eve.CMD_REGWRITE(eve.REG_SC0_PTR0, 0x7d80000 - scrsize)
+            eve.CMD_REGWRITE(eve.REG_SC0_PTR1, 0x7d80000 - (2 * scrsize))
             eve.LIB_EndCoProList()
             eve.LIB_AwaitCoProEmpty()
 
