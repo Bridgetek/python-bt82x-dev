d13319669358f8acae8e95bfaafb21d219aa1b1f
Gordon McNab <gordon.mcnab@brtchip.com>
2025-10-30 16:16:07 +0000

    Fix indentation issue.

--- bteve2/_eve.py
+++ bteve2/_eve.py
@@ -9,14 +9,14 @@ class _EVE:
     # It is only sent when flush is called or the buffer exceeds
     # the size of the FIFO.
     def cc(self, s):
+        print(f"cc {self.bufptr}")
         assert (len(s) % 4) == 0, "Coprocessor commands must be a multiple of 4 bytes"
         for b in s: 
             self.buf[self.bufptr] = b
             self.bufptr += 1
-        #assert (self.bufptr % 4) == 0, "Coprocessor command buffer must be a multiple of 4 bytes"
-        # Flush the co-processor buffer to the EVE device.
-        if self.bufptr >= self.FIFO_MAX - 16:
-            self.flush()
+            # Flush the co-processor buffer to the EVE device.
+            if self.bufptr >= self.FIFO_MAX - 16:
+                self.flush()
 
     # Send a 32-bit basic graphic command to the EVE.
     def cmd0(self, num):
@@ -28,6 +28,7 @@ class _EVE:
         self.cc(struct.pack(fmt, *args))
 
     def register(self, sub):
+        print(f"registering {self.FIFO_MAX}")
         self.buf = bytearray(self.FIFO_MAX)
         self.bufptr = 0
         assert (len(self.buf) == self.FIFO_MAX)
